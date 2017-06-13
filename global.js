8279426102da139e82ae7876355acece72538122
39Er <liuweiminfirst@163.com>
2017-06-13 11:52:41 +0800

    modify log time format

--- global.js
+++ global.js
@@ -3,6 +3,7 @@
 const bunyan = require('bunyan');
 const config = require('config');
 const path = require('path');
+const moment = require('moment');
 const Mystream = require('bunyan-rotate-file-stream');
 
 module.exports.config = config;
@@ -11,6 +12,7 @@ module.exports.logger = bunyan.createLogger({
   name: config.get('mainLog.name'),
   serializers: bunyan.stdSerializers,
   src: config.get('mainLog.src'),
+  time: moment().format('YYYY-MM-DDTHH:mm:ss'),
   streams: [
     {
       level: 'error',
